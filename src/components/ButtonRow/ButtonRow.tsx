abbb76de396c9d74f008af99065bdf7eff8915ac
tommy-mitchell <tmitchell7@uh.edu>
2023-09-22 20:40:00 -0500

    exercise 3

--- src/components/ButtonRow/ButtonRow.tsx
+++ src/components/ButtonRow/ButtonRow.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import clsx from "clsx";
 import styles from "./ButtonRow.module.scss";
 
 export type ButtonRowProps = Readonly<{
+	className?: string;
 	children: React.ReactNode;
 }>;
 
-export default function ButtonRow({ children }: ButtonRowProps) {
-	return <div className={styles.buttonRow}>{children}</div>;
+export default function ButtonRow({ className, children }: ButtonRowProps) {
+	return <div className={clsx(styles.buttonRow, className)}>{children}</div>;
 }
